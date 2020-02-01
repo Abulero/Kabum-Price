de50201d0f704099b4c02bf45ed70fce45195b48
Cecchi <andregcecchi@gmail.com>
2020-02-01 17:08:56 -0300

    Initial commit

---
+++ KabumPriceSearch.py
@@ -0,0 +1,24 @@
+from KabumScraping import KabumScraping
+import sys
+
+
+if __name__ == '__main__':
+    args_list = sys.argv.pop(0)
+    product = ''
+
+    if len(sys.argv) == 0:
+        print('Please type the name of a product as an argument')
+        sys.exit()
+
+    for arg in sys.argv:
+        product += arg
+
+    Kabum_Scraping = KabumScraping(product)
+
+    for i in range(10):
+        name, price = Kabum_Scraping.GetPrice(i)
+
+        if name == None:
+            break
+
+        print('-----{name}\n{price}\n'.format(name=name, price=price))
---
+++ KabumScraping.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+class KabumScraping:
+    def __init__(self, product):
+        product_url = product.replace(' ', '%20')
+
+        self.url = "https://www.kabum.com.br/cgi-local/site/listagem/listagem.cgi?ordem=3&limite=30&pagina=1&string={product}".format(product=product_url)
+        self.url_html = requests.get(self.url).content
+
+    def GetPrice(self, n):
+        self.soup = bs(self.url_html, 'html.parser')
+
+        name_element_list = self.soup.find_all('h2', class_='H-titulo')
+        price_element_list = self.soup.find_all('div', class_='listagem-preco')
+
+        if n > len(price_element_list) or n > len(name_element_list):
+            return None
+
+        return name_element_list[n].text, price_element_list[n].text
