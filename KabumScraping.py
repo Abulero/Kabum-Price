e04dc4a098ac1148d3d2bfdcb51a81b7a3255d1b
Abulero <49372191+Abulero@users.noreply.github.com>
2020-02-04 17:22:40 -0300

    Update KabumScraping.py

--- KabumScraping.py
+++ KabumScraping.py
@@ -9,7 +9,7 @@ class KabumScraping:
         self.url = "https://www.kabum.com.br/cgi-local/site/listagem/listagem.cgi?ordem=3&limite=30&pagina=1&string={product}".format(product=product_url)
         self.url_html = requests.get(self.url).content
 
-    def GetPrice(self, n):
+    def get_price(self, n):
         self.soup = bs(self.url_html, 'html.parser')
 
         name_element_list = self.soup.find_all('h2', class_='H-titulo')
